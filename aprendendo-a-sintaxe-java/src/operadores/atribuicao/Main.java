9ef1c3ca00a8c5de8211e805bb3743a6e0835479
IcaroRaphael <icaro2551@gmail.com>
2023-08-29 23:50:39 -0300

    feat: Operador de Atribuição

---
+++ aprendendo-a-sintaxe-java/src/operadores/atribuicao/Main.java
@@ -0,0 +1,13 @@
+package operadores.atribuicao;
+
+public class Main {
+    public static void main(String[] args) {
+        //classe Operadores.java
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+//        Date dataNascimento = new Date();
+    }
+}
