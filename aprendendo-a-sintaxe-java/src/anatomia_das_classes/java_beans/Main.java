047f746061095cf34e0fbac44969c1d5f356ea25
IcaroRaphael <icaro2551@gmail.com>
2023-08-29 23:39:29 -0300

    feat: Java Beans

---
+++ aprendendo-a-sintaxe-java/src/anatomia_das_classes/java_beans/Main.java
@@ -0,0 +1,34 @@
+package anatomia_das_classes.java_beans;
+
+public class Main {
+    public static void main(String[] args) {
+        // VARIÁVEIS
+
+        /*
+        Não recomendado
+
+        double salMedio = 1500.23  //variável abreviada, o que dificulta a compreensão
+        String emails = "aluno@escola.com" //confuso se a variável seria um array ou único e-mail
+        String myName = "JOSEPH" //se idioma pt-BR, o valor poder ser de outro idioma mas o nome da variável não
+        */
+
+        //Recomendado
+        double salMedio = 1500.23;  //variável abreviada, o que dificulta a compreensão
+        String emails = "aluno@escola.com"; //confuso se a variável seria um array ou único e-mail
+        String myName = "JOSEPH"; //se idioma pt-BR, o valor poder ser de outro idioma mas o nome da variável não
+
+
+        // MÉTODOS
+        /*
+        somar(int n1, int n2){}
+
+        abrirConexao(){}
+
+        concluirProcessamento() {}
+
+        findById(int id){} // não se assuste, você verá muito método em inglês em sua jornada
+
+        calcularImprimir(){} // há algo de errado neste método, ele deveria ter uma única finalidade
+        */
+    }
+}
