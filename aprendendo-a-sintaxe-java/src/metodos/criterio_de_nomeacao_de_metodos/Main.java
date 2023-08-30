f22bdfc8aa5731bb6f589494fd981290ab9ed167
IcaroRaphael <icaro2551@gmail.com>
2023-08-30 16:13:32 -0300

    feat: Critério de nomeação de Métodos

---
+++ aprendendo-a-sintaxe-java/src/metodos/criterio_de_nomeacao_de_metodos/Main.java
@@ -0,0 +1,16 @@
+package metodos.criterio_de_nomeacao_de_metodos;
+
+public class Main {
+    public static void main(String[] args) {
+//        somar(int n1, int n2){}
+//
+//        abrirConexao(){}
+//
+//        concluirProcessamento() {}
+//
+//        findById(int id){} // não se assuste, você verá muito método em inglês em sua jornada
+//
+//        calcularImprimir(){} // há algo de errado neste método, ele deveria ter uma única finalidade
+
+    }
+}
