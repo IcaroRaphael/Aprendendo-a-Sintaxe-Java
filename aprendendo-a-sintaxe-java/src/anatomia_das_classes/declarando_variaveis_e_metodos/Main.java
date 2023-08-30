4a4004ca10001cb16d9ccad862ff21017b20c108
IcaroRaphael <icaro2551@gmail.com>
2023-08-29 23:31:30 -0300

    feat: Declarando variáveis e métodos

---
+++ aprendendo-a-sintaxe-java/src/anatomia_das_classes/declarando_variaveis_e_metodos/Main.java
@@ -0,0 +1,32 @@
+package anatomia_das_classes.declarando_variaveis_e_metodos;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Declarar uma variável em Java segue sempre a seguinte estrutura:
+
+        // Estrutura
+        Tipo NomeBemDefinido = Atribuicao (opcional em alguns casos)
+
+        // Exemplo
+
+        int idade = 23;
+        double altura = 1.62;
+        Dog spike; //observe que aqui a variável spike não tem valor, é normal
+        */
+
+        /*
+        Declarar métodos em Java segue uma estrutura bem simples:
+
+        // Estrutura
+
+        TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+        //Exemplo
+
+        int somar (int numeroUm, int numero2)
+
+        String formatarCep (long cep)
+        */
+    }
+}
