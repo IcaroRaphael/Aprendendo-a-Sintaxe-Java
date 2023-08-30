88752a2274769a70b03d52fb4ec2feeaadf23a0d
IcaroRaphael <icaro2551@gmail.com>
2023-08-29 23:52:24 -0300

    feat: Operadores Aritméticos

---
+++ aprendendo-a-sintaxe-java/src/operadores/aritmeticos/Main.java
@@ -0,0 +1,29 @@
+package operadores.aritmeticos;
+
+public class Main {
+    public static void main(String[] args) {
+        //classe Operadores.java
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        //classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?";
+
+        concatenacao = 1+1+1+"1";
+
+        concatenacao = 1+"1"+1+1;
+
+        concatenacao = 1+"1"+1+"1";
+
+        concatenacao = "1"+1+1+1;
+
+        concatenacao = "1"+(1+1+1);
+    }
+}
